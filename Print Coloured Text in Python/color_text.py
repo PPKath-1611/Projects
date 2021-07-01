636937b82b56fe7061fe201373fc5c24ce3a31c1
Maverick <pranesh.kathavate@gmail.com>
2021-07-01 12:56:23 +0530

    Print Coloured Text in Python 1C

---
+++ Print Coloured Text in Python/color_text.py
@@ -0,0 +1,7 @@
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(autoreset=True)
+
+print(Fore.BLUE+Back.YELLOW+"Hi My name is Pranesh Kathavate "+ Fore.YELLOW+ Back.BLUE+"I am Data Science Enthusiast")
+print(Back.RED+"Hi I'm Aerodynamic")
+print(Fore.RED + Back.GREEN+ "Hi My name is Pranesh Kathavate")
